859c7cc24f38ad8997925bc38dac0bf51f87d998
Andreas Westerlind <andreas@westerlind.me>
2021-09-11 13:16:01 +0200

    Change

--- client_code.go
+++ client_code.go
@@ -3,5 +3,5 @@ package main
 // The client_id and client_secret values are specific to the client application,
 // and needed for the authentication process to function.
 // You can get yours from https://app.raindrop.io/#/settings/apps/dev
-var client_id string = "5e46fab9b2fbaee7314687d8"
-var client_secret string = "55b5a4a1-f1eb-4e38-a4a9-05b0b26ba619"
+var client_id string = ""
+var client_secret string = ""
